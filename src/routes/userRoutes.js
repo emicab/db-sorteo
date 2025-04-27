0a581a94fd24ed4aec4e35298ef2102700b9faef
emicab <emicabanillas.cab@gmail.com>
2025-04-27 16:35:55 -0300

    cambios, mejoras

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@ import { registerUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/register-notfound", registerUser);
+// router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 export default router;
